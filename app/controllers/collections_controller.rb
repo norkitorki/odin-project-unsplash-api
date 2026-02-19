6853f69b495e13c9f216a93f87f64fa962b655e9
norkitorki <norki@tuta.io>
2026-02-19 12:38:13 +0000

    Create CollectionsController and configure show route

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,23 @@
+class CollectionsController < ApplicationController
+  def show
+    api_key = Rails.application.credentials.unsplash_api_key
+    @collection_id = params[:collection_id]
+
+    response = Faraday.get(
+      "https://api.unsplash.com/collections/#{@collection_id}/photos",
+      nil,
+      {
+        Authorization: "Client-ID #{api_key}",
+        "Content-Type": "application/json"
+      }
+    )
+
+    if response.status == 200
+      flash.now[:notice] = "Collection successfully retrieved"
+      @photos = JSON.parse(response.body)
+      render "home/index"
+    else
+      redirect_to root_path, alert: "Unable to retrieve collection: #{response.reason_phrase}"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "home/index"
+  get "/collection", to: "collections#show"
   root "home#index"
 end
